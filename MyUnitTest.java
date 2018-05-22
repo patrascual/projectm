a12ef0e7521069a2edd6953f33784bcb2159c73f
Patrascu <alexandru.patrascu@nokia.com>
2018-05-22 07:27:57 -0400

    API test using TestNG

--- MyUnitTest.java
+++ MyUnitTest.java
@@ -1,13 +1,5 @@
-/**
- * This tests the java.net.URL APIs.
- * Methods that are implemented in the API and who's functionality is tested: 
- * getFile - Return the file name of URL, or an empty string if one does not exist
- * openConnection - Returns a URLConnection instance that represents a connection to the remote object referred to by the URL
- * Class MalformedURLException - to indicate that a malformed URL has occurred
- * 
- * 
- */
 package testNG;
+
 import static org.testng.Assert.assertEquals;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -18,61 +10,76 @@ import java.net.URL;
 
 import org.apache.commons.io.*;
 
+public class MyUnit {
+	public class MyUnitTest {
+		
+		//Provide a number of reachable URLs 
+		@DataProvider (name = "reachableURLs")
+		public String[] list1() {
+			return new String[] {"https://www.oracle.com", "http://abc.com"};
+		}
+		
+	    //Provide a number of unreachable URLs
+	    @DataProvider (name = "unreachableURLs")
+		public String[] list2() {
+			return new String[] {"https://www.oracle.com/nofile.asdf", "http://abc.com/nofile.asdf"};
+		}
+	    
+	    //Provide a number of invalid URLs
+	    @DataProvider (name = "invalidURLs")
+		public String[] list3() {
+			return new String[] {"httq://www.google.com", "http//www.google.com", "http/www.google.com"};
+		}
+	   
+	    @Test()
+	    public void getFile_ValidInput_FileIsReturned() throws MalformedURLException {
+	        
+	    	URL url = new URL("http://www.test.com/myfile.txt");
+	        String result = url.getFile();
+	            	       
+	        assertEquals(result, "/myfile.txt");
+	    }
+	    
+	    @Test()
+	    public void getFile_InvalidInput_EmptyStringReturned() throws MalformedURLException {
+	        
+	    	URL url = new URL("http://www.test.com");
+	        String result = url.getFile();
+	            	       
+	        assertEquals(result, "");
+	    }
+	    
+	    //Input for testConnection is provided by the dataprovider "reachableURLs"
+	    //This method tests if a wedaddress is reachable (HTTP 200 OK)
+	    @Test(dataProvider = "reachableURLs")
+	    public void connect_ReachableAddress_Http200Response(String webaddress) throws Exception{
+	    	
+	    	URL url = new URL(webaddress);
+	    	
+	    	try {
+	    	        HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+	    	        urlConn.connect();
 
-public class MyUnitTest {
-	
-	//Provide a number of URLs that can be tested for connectivity purposes
-	@DataProvider (name = "files")
-	public static String[] list1() {
-		return new String[] {"https://docs.oracle.com/javase/7/docs/api/java/net/URL.html"};
+	    	        assertEquals(HttpURLConnection.HTTP_OK, urlConn.getResponseCode());
+	    	    } catch (IOException e) {
+	    	        System.err.println("Error accessing the site");
+	    	        e.printStackTrace();
+	    	        throw e;
+	    	    }
+	    }
+	    
+	    @Test(dataProvider = "unreachableURLs")
+	    public void connect_UnreachableAddress_Http404Response(String webaddress) throws Exception{
+	    	
+	        // .... similar with connect_ReachableAddress_Http200Response but different input
+	    	
+	    }
+
+	    @Test(dataProvider = "invalidURLs", expectedExceptions = MalformedURLException.class, description="Test for MalformedURLException due to invalid input for constructor")
+	    public void urlConstructor_InvalidUrlInput_MalformedExceptionThrown(String webaddress) throws Exception {
+	    
+	    	    URL url = new URL(webaddress);
+	    }
 	}
-	
-	@DataProvider (name = "URLs")
-	public static String[] list2() {
-		return new String[] {"https://www.oracle.com", "http://abc.com"};
-				
-	}
-	
-    @Test(dataProvider = "files", enabled = true)
-    public static void testgetfile(String webaddress) throws Exception {
-        
-    	URL url = new URL(webaddress);
-    	//result should have only the string "URL", as a result of getBaseName on the getFiel method
-        String result = FilenameUtils.getBaseName(url.getFile());
-            	
-        // check to see if the result matches the expected value
-        
-        assertEquals(result, "URL");
-    }
-    
-    //Input for testConnection is provided by the dataprovider "URLs"
-    //This method tests if a wedaddress is reachable (HTTP 200 OK)
-    @Test(priority= 1, dataProvider = "URLs")
-    public void testConnection(String webaddress) throws Exception{
-    	
-    	URL url = new URL(webaddress);
-    	
-    	try {
-    	        HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
-    	        urlConn.connect();
-
-    	        assertEquals(HttpURLConnection.HTTP_OK, urlConn.getResponseCode());
-    	    } catch (IOException e) {
-    	        System.err.println("Error accessing the site");
-    	        e.printStackTrace();
-    	        throw e;
-    	    }
-
-    }
-    // Tested exception for a malformed URL: wrong protocol
-    @Test(expectedExceptions = MalformedURLException.class, description="Test for MalformedURLException - unknown protocol ")
-    public void testmalformed() throws Exception{
-    
-    	    	URL url = new URL("httq://www.google.com");
-    			HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
-    	        urlConn.connect();
-    			
-    		
-    }
-
 }
+
